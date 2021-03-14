ac8d1c56c20f4dedd2f26aa9b868d6a1c82069a4
Kary-AG <kary_agarcia@ciencias.uanm.mx>
2021-03-13 20:08:35 -0600

    rotate

---
+++ 2021-2/march/RotateArray.java
@@ -0,0 +1,23 @@
+public class RotateArray{
+    public static void main(String[] args){
+        int[] nums ={1,2,3,4,5,6,7};
+        int k =3;
+        rotate(nums, k);
+    }
+    public static int[] rotate(int[] nums , int k){
+        for(int i=0; i<k; i++){
+            int ylast = nums[nums.length-1];
+            for(int j =nums.length-1;j>0;j--)
+                nums[j]=nums[j-1];
+            nums[0]=last;
+        }
+        return nums;
+
+    }
+
+    public static void println(int[] array){
+        for(int i = 0; i<array.length; i++)
+            System.out.println(array[i]);
+    }
+    
+}
