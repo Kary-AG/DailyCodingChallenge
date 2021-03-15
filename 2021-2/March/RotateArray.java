8189698418168f946badafce92bdf6ea48705ebc
Karyme García <kary_agarcia@ciencias.unam.mx>
2021-03-14 21:03:27 -0600

    Update RotateArray.java

--- 2021-2/March/RotateArray.java
+++ 2021-2/March/RotateArray.java
@@ -6,7 +6,7 @@ public class RotateArray{
     }
     public static int[] rotate(int[] nums , int k){
         for(int i=0; i<k; i++){
-            int ylast = nums[nums.length-1];
+            int last = nums[nums.length-1];
             for(int j =nums.length-1;j>0;j--)
                 nums[j]=nums[j-1];
             nums[0]=last;
